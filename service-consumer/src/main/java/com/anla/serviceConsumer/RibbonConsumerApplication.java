308b327518c1c48ac5023cf3080d300f91ad2211
anLA7856 <luoan007@gmail.com>
2018-06-11 21:05:16 +0800

    Revert "hystrix added"
    This reverts commit c4e9773

--- service-consumer/src/main/java/com/anla/serviceConsumer/RibbonConsumerApplication.java
+++ service-consumer/src/main/java/com/anla/serviceConsumer/RibbonConsumerApplication.java
@@ -6,7 +6,6 @@ package com.anla.serviceConsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -15,10 +14,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker
 public class RibbonConsumerApplication {
 
     public static void main(String[] args){
         SpringApplication.run(RibbonConsumerApplication.class,args);
     }
+
+
 }
